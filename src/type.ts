906ecaa4f988f51471d459425ece9c1a57df10cf
pierre pracht <pierre.pracht@gmail.com>
2022-09-11 18:18:08 +0200

    type typescript "simple"

---
+++ src/type.ts
@@ -0,0 +1,9 @@
+export interface Poems {
+  id?: number;
+  created_at?: string | null;
+  title?: string | null;
+  content?: string | null;
+  email?: string | null;
+  illustrationurl?: string | null;
+  hidden?: boolean | null;
+}
