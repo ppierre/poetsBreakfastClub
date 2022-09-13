62f5ed6b98942b9fa989e66db6003abe16d13aee
pierre pracht <pierre.pracht@gmail.com>
2022-09-13 12:41:39 +0200

    ajout signUp et bascule singIn/signUP par ref booléen

--- src/pages/login-logout.vue
+++ src/pages/login-logout.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
+import { ref } from "@vue/reactivity";
 import { supabase, user } from "../supabase";
 async function signIn(data) {
-  const { user, error } = await supabase.auth.signIn(data);
+  debugger;
+  const { user, error } = await (nvlUtilisateur
+    ? supabase.auth.signUp(data)
+    : supabase.auth.signIn(data));
   if (error) console.error(error);
 }
+const nvlUtilisateur = ref(false);
 </script>
 <template>
   <div>
@@ -13,11 +18,17 @@ async function signIn(data) {
     <FormKit
       v-else
       type="form"
-      submit-label="Se connecter ou s'inscrire"
+      :submit-label="nvlUtilisateur ? 'S\'inscrire' : 'Se connecter'"
       @submit="signIn"
     >
       <FormKit name="email" label="Votre eMail" type="email" />
       <FormKit name="password" label="Mot de passe" type="password" />
+      <formKit
+        label="Nouvel utilisateur ?"
+        name="nvlUtilisateur"
+        type="checkbox"
+        v-model="nvlUtilisateur"
+      />
     </FormKit>
   </div>
 </template>
